77034ff45ce9ad55499a711321d0c23c2e4c993e
l3miage-severinm <matteo.severini@etu.univ-grenoble-alpes.fr>
2024-04-05 14:15:35 +0200

    Création de la classe CandidateRepositoryTest

---
+++ server/src/test/java/fr/uga/l3miage/spring/tp3/repositories/CandidateRepositoryTest.java
@@ -0,0 +1,5 @@
+package fr.uga.l3miage.spring.tp3.repositories;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK, properties = "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect")
+public class CandidateRepositoryTest {
+}
